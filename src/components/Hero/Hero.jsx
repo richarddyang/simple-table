822346c50089d95f2fdd7ae8e243a2e2e50c87de
dangrichardyang <richarddangyang@gmail.com>
2022-12-14 13:23:22 -0600

    add: client side pagination

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,41 @@
-import results from "../../assets/MOCK_DATA_SMALL.json";
+import { useState } from "react";
+
+import "./Hero.css";
+import data from "../../assets/MOCK_DATA_FULL.json";
 import Table from "../Table/Table";
+import Pagination from "../Pagination/Pagination";
 
 const Hero = () => {
-  console.log(results);
-  return <Table results={results} />;
+  ///////////////////////////STATE INFO///////////////////////////////////////////////
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentLimit, setCurrentLimit] = useState(10);
+
+  ///////////////////////////FUNCTIONS////////////////////////////////////////////
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleLimitChange = (e) => {
+    setCurrentLimit(parseInt(e.target.value));
+  };
+
+  return (
+    <div className="hero_container">
+      <Table
+        results={data.slice(
+          (currentPage - 1) * currentLimit,
+          (currentPage - 1) * currentLimit + currentLimit,
+        )}
+      />
+      <Pagination
+        page={currentPage}
+        limit={currentLimit}
+        totalResults={data ? data.length : 0}
+        handlePageChange={handlePageChange}
+        handleLimitChange={handleLimitChange}
+      />
+    </div>
+  );
 };
 
 export default Hero;

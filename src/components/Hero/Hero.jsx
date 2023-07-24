dee8abcc36341258b59746e3b29e718e791e9b82
richarddyang <richard.yang@skywatertechnology.com>
2023-07-24 17:26:20 -0500

    add: base url and environment variables

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import "./Hero.css";
 import Table from "../Table/Table";
 import Pagination from "../Pagination/Pagination";
 
-const API_URL = "http://localhost:5000"
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"
 
 const Hero = () => {
   ///////////////////////////STATE INFO///////////////////////////////////////////////
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    plugins: [react()],
+    base: env.VITE_CLENT_BASE || "/"
+  }
 })
